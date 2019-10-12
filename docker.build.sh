b250682a585bfaa6bc884104f520e1ddce9a12dc
jingtian60 <lncdyx@tom.com>
2019-10-12 14:43:57 +0800

    init

---
+++ docker.build.sh
@@ -0,0 +1,5 @@
+docker build ./docker/rocketmq-base -t szss/rocketmq-base:v4.3.1
+
+docker build ./docker/rocketmq-broker -t szss/rocketmq-broker:v4.3.1
+
+docker build ./docker/rocketmq-namesrv -t szss/rocketmq-namesrv:v4.3.1
