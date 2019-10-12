82335b197cdaa8584c29ea7abc4504e0360e7113
jingtian60 <lncdyx@tom.com>
2019-10-12 15:16:35 +0800

    a

--- kubernetes/nfs_export.sh
+++ kubernetes/nfs_export.sh
@@ -1,5 +1,5 @@
-cp /etc/export /etc/export.bak
-cat >> /etc/export  <<EOF
+cp /etc/exports /etc/exports.bak
+cat >> /etc/exports  <<EOF
 /nfs/data/store-b *(rw,sync,no_subtree_check,no_root_squash)
 /nfs/data/store-b-s *(rw,sync,no_subtree_check,no_root_squash)
 EOF
