cf3b1a78f4a06ece3c7df21228b7e9f076de8010
jingtian60 <lncdyx@tom.com>
2019-10-12 15:20:27 +0800

    修复

--- kubernetes/nfs_export.sh
+++ kubernetes/nfs_export.sh
@@ -1,6 +1,6 @@
 cp /etc/exports /etc/exports.bak
 cat >> /etc/exports  <<EOF
-/nfs/data/store-b *(rw,sync,no_subtree_check,no_root_squash)
-/nfs/data/store-b-s *(rw,sync,no_subtree_check,no_root_squash)
+/nfs/data/rocketmq/store-b *(rw,sync,no_subtree_check,no_root_squash)
+/nfs/data/rocketmq/store-b-s *(rw,sync,no_subtree_check,no_root_squash)
 EOF
 exportfs -r
